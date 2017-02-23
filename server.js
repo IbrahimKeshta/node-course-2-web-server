44fe1909560d4e8c1b79f6892b4d3454bb90f5c1
IbrahimKeshta <keshta95@hotmail.com>
2017-02-23 22:29:14 +0200

    =Initial commit

---
+++ server.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+// new express app
+var app = express();
+
+
+// Partials is to include partial like footer in any file 
+
+hbs.registerPartials(__dirname + '/views/partials');
+//handlebarsjs => set view engine
+app.set('view engine', 'hbs');
+
+//middleware 
+
+app.use(express.static(__dirname + '/public'));
+
+// register middleware
+app.use((req, res, next) => { // next is when it done
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log');
+        }
+    });
+  next();
+});
+
+// Chalenge
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs');
+// });
+
+//middleware 
+// it must be after maintenance middleware to not execute help.html
+app.use(express.static(__dirname + '/public'));
+
+// handlebars helper
+//register function to run to create some output
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+
+// seeting up all http handlers 
+// register handler
+app.get('/', (req, res) => {
+//  res.send('Hello Express!'); // respones for http request
+    // res.send({
+    //     name: 'Ibrahim Keshta',
+    //     like: [
+    //         'eating',
+    //         'music',
+    //         'coding'
+    //     ]
+    // });
+    //Chalenge
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        // currentYear: new Date().getFullYear(),
+        welcomeMessage: 'Welcome to our website'
+    });
+});
+app.get('/about', (req, res) => {
+    // res.send('About Page');// root route
+    // render any of templates you have setup with view engine
+    // we will passing data to about.hbs  
+     res.render('about.hbs', {
+         pageTitle: 'About Page',
+        //  currentYear: new Date().getFullYear()
+     }); 
+})
+
+// /bad
+app.get('/bad' , (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+})
+
+
+
+//start listen will bind app to a port on the machine
+app.listen(3000, () => {
+    console.log('server is up on port 3000');
+} );
